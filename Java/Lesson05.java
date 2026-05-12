f2415ec2ef93da073b4da0e10152aec116d62766
hajikko25ykkm-del <hajikko25ykkm@gmail.com>
2026-05-13 00:12:48 +0900

    代入演算子、論理演算子

---
+++ Java/Lesson05.java
@@ -0,0 +1,44 @@
+// 関係演算子  二つの値が正しかを比較する
+// > より大きい
+// < より小さい
+// >= より大きいか等しい
+// <= より小さいか等しい
+// == 等しい
+// != 等しくない
+
+// 論理演算子  二つの値が両方とも正しいかを比較する
+// && 両方とも正しい
+// || どちらかが正しい
+
+// 代入演算子  変数に値を代入する
+// = 代入
+// += 足す
+// -= 引く
+// *= 掛ける
+// /= 割る
+// %= 余剰
+
+// インクリメントとデクリメント
+// ++ 増やす
+// -- 減らす
+
+class Lesson05 {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 2;
+    int z = 10;
+
+    System.out.println(x == y);  // 等しければtrueになるが、xはyと等しくないのでfalseになる
+    System.out.println(x != y);  // 等しくなければtrueになるが、xはyと等しくないのでtrueになる
+
+    System.out.println(x >= 5 && x <= 15);  // どちらも正しいのでtrueになる
+    System.out.println(y >= 5 && y <= 15);  // どちらも正しくないのでfalseになる
+
+    System.out.println(x == 10 || y == 10);  // 片方が正しいのでtrueになる
+    System.out.println(x == 5 || y == 5);  // どちらも正しくないのでfalseになる
+
+    System.out.println(x += 5);
+    System.out.println(z += y);
+
+  }
+}
