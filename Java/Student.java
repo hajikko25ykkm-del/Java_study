3a2a90d3ecf099d36b91b9b16398f35623ec727c
hajikko25ykkm-del <hajikko25ykkm@gmail.com>
2026-05-22 23:48:23 +0900

    設計図クラス、実行クラスの設定

--- Java/Student.java
+++ Java/Student.java
@@ -1,4 +1,4 @@
-public class Object01 {
+public class Student {
   // メンバ変数
   String name;
   int engScore;
@@ -17,7 +17,23 @@ public class Object01 {
     double avg = (engScore + mathScore ) / 2.0; // 2.0で割ることで小数点以下も表示
     return avg;                                 // returnはメソッドの処理結果を呼び出し元に返す
   }
+
+  public static void main(String[] args) {
+    Student obj1 = new Student();
+    Student obj2 = new Student();
+
+    obj1.name = "田中";
+    obj2.name = "山田";
+    obj1.setScore(90, 80);
+    obj2.setScore(65, 70);
+    
+    obj1.display();
+    System.out.println("平均" + obj1.getAvg() + "点");
+    obj2.display();
+    System.out.println("平均" + obj2.getAvg() + "点");
+  }
 }
+
 
 /*オブジェクト指向、設計書と実行を分ける
 修正や機能追加がらくちん、情報ごとにクラス分けしてるので処理が早い*/
@@ -26,3 +42,6 @@ public class Object01 {
 // voidは返り値がない、持っているデータを表示する
 
 // 戻り値はメソッドの処理結果を呼び出し元に返す値（return）
+
+// コンパイルはメインメソッドが含まれているクラスを指定する
+// 設計図の方は一緒にコンパイルしてもらえるので、分けてコンパイルしなくてよい！
